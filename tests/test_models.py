8e0c88a93090b407a6be810f32ef5da3e279de2d
Adam Slater <adam.slater@ukaea.uk>
2024-06-04 13:42:03 +0100

    add tests for daily_max daily_min

--- tests/test_models.py
+++ tests/test_models.py
@@ -1,8 +1,11 @@
 """Tests for statistics functions within the Model layer."""
 
+import os
+
 import numpy as np
 import numpy.testing as npt
-import os
+
+import pytest
 
 
 def test_daily_mean_zeros():
@@ -37,3 +40,26 @@ def test_load_from_json(tmpdir):
         temp_json_file.write('[{"observations":[1, 2, 3]},{"observations":[4, 5, 6]}]')
     result = load_json(example_path)
     npt.assert_array_equal(result, [[1, 2, 3], [4, 5, 6]])
+
+def test_daily_max():
+    from inflammation.models import daily_max
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([5, 6])
+    npt.assert_array_equal(daily_max(test_input), test_result)
+
+def test_daily_min():
+    from inflammation.models import daily_min
+    test_input = np.array([[1, 2],
+                           [3, 4],
+                           [5, 6]])
+    test_result = np.array([1, 2])
+    npt.assert_array_equal(daily_min(test_input), test_result)
+
+def test_daily_min_string():
+    """Test for TypeError when passing strings"""
+    from inflammation.models import daily_min
+
+    with pytest.raises(TypeError):
+        error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
