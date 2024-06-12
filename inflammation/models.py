9be36c77b9ac535135cc1c84d6315546cf1f3155
Adam Slater <adam.slater@ukaea.uk>
2024-06-12 13:45:12 +0100

    add patient normalise and tests

--- inflammation/models.py
+++ inflammation/models.py
@@ -38,8 +38,6 @@ def load_json(filename):
         data_as_json = json.load(file)
         return [np.array(entry['observations']) for entry in data_as_json]
 
-
-
 def daily_mean(data):
     """Calculate the daily mean of a 2D inflammation data array."""
     return np.mean(data, axis=0)
@@ -53,3 +51,21 @@ def daily_max(data):
 def daily_min(data):
     """Calculate the daily min of a 2d inflammation data array."""
     return np.min(data, axis=0)
+
+def patient_normalise(data):
+    """
+    Normalise patient data from a 2D inflammation data array.
+
+    NaN values are ignored, and normalised to 0.
+
+    Negative values are rounded to 0.
+    """
+    if np.any(data < 0):
+        raise ValueError('Inflammation values should not be negative')
+
+    maxima = np.nanmax(data, axis=1)
+    with np.errstate(invalid='ignore', divide='ignore'):
+        normalised = data / maxima[:, np.newaxis]
+    normalised[np.isnan(normalised)] = 0
+    normalised[normalised < 0] = 0
+    return normalised
